2eceae0539e16a026cdd6c596896700c11ad01ab
Alberto <alberto.abate87@gmail.com>
2021-01-21 17:07:07 +0100

    creazione btn index.blade.php

---
+++ resources/views/classrooms/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layout.main')
+
+@section('content')
+    <div class="container mb-5">
+        <h1>Our classrooms</h1>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($classrooms as $classroom)
+                    <tr>
+                        <td>{{ $classroom->id }}</td>
+                        <td>{{ $classroom->name }}</td>
+                        <td class="text-center" width="100">
+                            <a href="{{ route('classrooms.show', $classroom->id) }}" class="btn btn-success">show</a>
+                        </td>
+                        <td class="text-center" width="100">
+                            <a href="#" class="btn btn-primary">edit</a>
+                        </td>
+                        <td class="text-center" width="100">
+                            <a href="#" class="btn btn-danger">delete</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.main')
+
+@section('content')
+    <div class="container mb-5">
+        <h1>school homepage here</h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga animi, fugit corporis provident quas dolorem. At repellat neque quis ratione voluptatum nam error ea molestiae mollitia corrupti, facere unde rem!</p>
+    </div>
+@endsection
